6dfd41b1fbdaa9f28ddf250543712ecabac9d530
farhan <muhammadfarhan3789076@gmail.com>
2025-07-09 11:33:19 +0500

    init

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigationType } from "react-router";
 import ResponsiveNavbar from "../components/Shared/Navbar/ResponsiveNavbar";
+import NProgress from "../progress";
+import { useEffect } from "react";
 
 function MainLayout() {
+    const location = useLocation();
+
+  useEffect(() => {
+    NProgress.start();
+
+    // slight delay to simulate loading — real apps might use data fetching triggers here
+    const timeout = setTimeout(() => {
+      NProgress.done();
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [location]);
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
       <ResponsiveNavbar />
